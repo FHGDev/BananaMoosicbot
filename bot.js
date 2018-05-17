9afe89b9146d56e2cc7406f944765f451490f251
FHGDev <gideon.lingle14@gmail.com>
2018-05-16 19:01:07 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const request = require('request')
 const fs = require('fs')
 const getYouTubeID = require('get-youtube-id')
 const fetchVideoInfo = require('youtube-info')
-const controllerrole;
+const controllerrole = ""
 bot.version = require('package.json').version
 bot.commands = new discord.Collection()
 bot.invite = "https://discordapp.com/api/oauth2/authorize?client_id=446450558585405440&permissions=0&scope=bot"
