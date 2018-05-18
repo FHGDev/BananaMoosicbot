756875dbf5f9d26bd7a556806b90be896235fde6
FHGDev <gideon.lingle14@gmail.com>
2018-05-17 20:34:12 -0500

    Create play.js

---
+++ commands/play.js
@@ -0,0 +1,19 @@
+const Music = require("discord.js-musicbot-addon")
+
+module.exports.run = (bot, message, args) => {
+  const moosic = new Music(bot, {
+    prefix: bot.prefix,
+    leaveHelp: "Nooooes rip me.",
+    helpCmd: bot.commands.get('help').run(bot, message, args),
+    maxQueueSize: 10,
+    disableLoop: false,
+    botOwner: "242734840829575169",
+    youtubeKey: process.env.ytapikey
+  })
+}
+
+module.exports.help = {
+  name: "play",
+  usage: "b!play [song name] or b!play [song link]",
+  info: "Play some sw33t tunes"
+}
