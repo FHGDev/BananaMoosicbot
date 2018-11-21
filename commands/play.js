404db07173086e924d6816a00f6798aba46d3860
FHGDev <gideon.lingle1@gmail.com>
2018-11-21 15:41:18 -0500

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,16 +1,7 @@
-const Music = require("discord.js-musicbot-addon")
-const ffmpeg = require('ffmpeg')
-
+var queue = []
+var ytdl = require('ytdl-core')
 module.exports.run = (bot, message, args) => {
-  Music.start(bot, { 
-    prefix: bot.prefix,
-    leaveHelp: "Nooooes rip me.",
-    helpCmd: bot.commands.get('help').run(bot, message, args),
-    maxQueueSize: 10,
-    disableLoop: false,
-    botOwner: "242734840829575169",
-    youtubeKey: process.env.ytapikey
-  })
+  
 }
 
 module.exports.help = {
